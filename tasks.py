583c6e808f7ab523fae611f4df6ea434307fdd3b
Micah R Ledbetter <me@micahrl.com>
2025-01-31 19:46:27 -0600

    Add mypy task

--- tasks.py
+++ tasks.py
@@ -6,3 +6,10 @@ def cog(ctx):
     """Run cog -r readme.md using the cogapp API."""
 
     ctx.run("cog -r readme.md")
+
+
+@task
+def mypy(ctx):
+    """Run mypy on the src directory."""
+
+    ctx.run("mypy src")
